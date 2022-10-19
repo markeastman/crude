6518815edf88024503ffaf44e01681b42a56db04
meastman <mark@eastmans.me.uk>
2022-10-19 14:07:32 +0100

    Displays a list of entities with some very basic table layout

--- CrudeWeb/src/main/java/uk/me/eastmans/servlet/listEntityServlet.java
+++ CrudeWeb/src/main/java/uk/me/eastmans/servlet/listEntityServlet.java
@@ -3,6 +3,10 @@ package uk.me.eastmans.servlet;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Query;
+import jakarta.persistence.metamodel.Attribute;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.ManagedType;
+import jakarta.persistence.metamodel.Metamodel;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,7 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 @WebServlet(
         name = "ListEntitiesServlet",
@@ -29,7 +35,24 @@ public class listEntityServlet extends HttpServlet {
         Query query = em.createQuery("select i from " + entityName + " i", Object.class);
         List results = query.getResultList();
 
-        req.setAttribute("entityName", entityName);
+        Metamodel metaModel = em.getMetamodel();
+        Set<ManagedType<?>> managedTypes = metaModel.getManagedTypes();
+        // We need to find the type requested so we can get the fields etc.
+        Iterator<ManagedType<?>> typesIterator = managedTypes.iterator();
+        while(typesIterator.hasNext()) {
+            ManagedType<?> type = typesIterator.next();
+            if (type instanceof EntityType<?>) {
+                EntityType entityType = (EntityType) type;
+                if (entityType.getName().equals( entityName )) {
+                    // We have the entity we want to output some column headers for it
+                    Set<Attribute> attributes = entityType.getAttributes();
+                    req.setAttribute("entityAttributes", attributes );
+                    break;
+                }
+            }
+        }
+        req.setAttribute("entityName", entityName );
+        req.setAttribute( "metaModel", metaModel );
         req.setAttribute("entityList", results );
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("listEntity.jsp" );
--- CrudeWeb/src/main/webapp/listEntity.jsp
+++ CrudeWeb/src/main/webapp/listEntity.jsp
@@ -2,15 +2,19 @@
 <%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <html xmlns = "http://www.w3c.org/1999/xhtml" xmlns:jsp = "http://java.sun.com/JSP/Page">
     <body>
-        <h2>List of ${entityName}</h2>
+        <h2>List of ${entityName} Entities</h2>
 
-        <table>
+        <table border="1" width="100%">
             <tr>
-                <th>todo</th>
-            <tr>
+            <c:forEach var="attribute" items="${entityAttributes}">
+                <th>${attribute.name}</th>
+            </c:forEach>
+            </tr>
         <c:forEach var="row" items="${entityList}">
             <tr>
-                <td>row</td>
+            <c:forEach var="attribute" items="${entityAttributes}">
+                <td>${row[attribute.name]}</td>
+            </c:forEach>
             </tr>
         </c:forEach>
         </table>
